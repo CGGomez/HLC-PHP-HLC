b73441fd9baa5e611b360eef5785543bc1ad06b0
César Gómez Gómez <55448868+CGGomez@users.noreply.github.com>
2021-01-20 16:50:35 +0100

    Add files via upload

---
+++ Tarea2.php
@@ -0,0 +1,18 @@
+<?php
+// Este comentario no se verá en la página
+ // Esto no se leerá
+/* Este es un comentario de
+múltiples líneas y no se acabará
+hasta que no cerremos así.... */
+
+# Este es un comentario tipo shell que tampoco se leerá
+# Este, tampoco
+#comentario invisible
+/* Cuidado con anidar
+ comentarios
+multilinea con estos
+al PHP no le gustan */
+
+echo "Esto se leerá <BR> ";
+echo "Este es el segundo comentario que se leerá<BR>"; 
+echo ("Aquí el tercer texto visible<BR");?>
---
+++ Tarea2b.php
@@ -0,0 +1,6 @@
+<?php
+
+print "Estoy haciendo pruebas"; //Es un constructor de lenguaje
+print("De PHP en HLC"); #Equivale a un print
+print_r "Nope"; //Esta es una función, va a dar un error
+echo"Supongo que lo estoy haciendo bien"; /*Escribe literal lo que tiene entre las comillas*/?>
