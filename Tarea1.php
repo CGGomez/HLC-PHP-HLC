478a67e6ec977be18e5997f7d951a4cccb2a8a88
César Gómez Gómez <55448868+CGGomez@users.noreply.github.com>
2021-01-20 16:37:55 +0100

    Add files via upload

--- Tarea1.php
+++ Tarea1.php
@@ -1,5 +1,5 @@
 <?php echo '<p>Hola Mundo</p>'; ?>
 
-<?php echo "Texto Random ha aparecido";?>
+<?php echo "Texto Random ha aparecido";?> /*Este puede imprimir por pantalla la cadena escrita*/
 
-<?php print_r "Un Texto Salvaje te ha atacado";?>
+<?php print_r "Un Texto Salvaje te ha atacado";?> /*Este en cambio solo puede imprimir Arrays y da un mensaje de error*/ 
