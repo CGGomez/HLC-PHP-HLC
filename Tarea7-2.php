eaf3231f7c753a32b9d41e1ad06441b959be472b
César Gómez Gómez <55448868+CGGomez@users.noreply.github.com>
2021-02-25 15:45:44 +0100

    Add files via upload

---
+++ Tarea7-2.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 5;
+$b = 6;
+
+$a == $b; // Ambas variables son iguales
+
+$a <= $b; // Variable a es menor o igual que la variable b
+
+$a >= $b; // Variable a es mayor o igual que la variable b
+
+$a === $b; //Variable a y b son iguales; comparten el mismo tipo
+
+$a > $b; // Variable a es mayor que la variable b
+
+$a < $b; // Variable a es mejor que la variable b
+
+$a != $b //Variable a es distinta de la variable b
+
+?>
